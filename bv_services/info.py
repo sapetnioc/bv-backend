0ebddf5c1ea2464a65e4deb753097c25eda905cd
sapetnioc <sapetnioc@users.noreply.github.com>
2021-10-27 18:25:14 +0200

    wip

---
+++ bv_services/info.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+version_major = 0
+version_minor = 0
+version_micro = 0
+version_extra = ''
+
+# Format expected by setup.py: string of form "X.Y.Z"
+__version__ = '{}.{}.{}{}'.format(version_major,
+                                  version_minor,
+                                  version_micro,
+                                  version_extra)
+# Main setup parameters
+NAME = 'bv_services'
+DESCRIPTION = 'Test project'
+VERSION = __version__
+LICENSE='unknown'
+REQUIRES = []
